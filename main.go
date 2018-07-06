e013e2ffaf2f16d1fbfad22706df79ac2a5a9358
ns-cweber <cweber@narrativescience.com>
2018-07-05 20:09:28 -0500

    Added support for encoding schemas as CSV files

--- main.go
+++ main.go
@@ -1,28 +1,16 @@
 package main
 
-import "log"
+import (
+	"log"
+)
 
 func main() {
-	if err := Validate(
-		FileSystemRepo{RootDirectory: "."},
-		[]Table{
-			Table{
-				Name: "items.csv",
-				Columns: []Column{
-					Column{
-						Name:       "id",
-						Type:       DataTypeInt{},
-						PrimaryKey: true,
-					},
-					Column{
-						Name:    "name",
-						Type:    DataTypeString{},
-						NotNull: true,
-					},
-				},
-			},
-		},
-	); err != nil {
-		log.Fatal(err)
+	schema, err := ParseSchema(".")
+	if err != nil {
+		log.Fatal("Failed to parse schema:", err)
+	}
+
+	if err := Validate(FileSystemRepo{"."}, schema); err != nil {
+		log.Fatal("Validation error:", err)
 	}
 }
